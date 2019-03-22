55b44fd31ac5cc0dc5231144cebd3f68ae643613
Chris Yee <christophery@gmail.com>
2019-03-22 13:01:23 -0400

    updated footer

--- footer.php
+++ footer.php
@@ -1,10 +1,15 @@
    <!-- {{!-- The footer at the very bottom of the screen --}} -->
         <footer class="site-footer outer">
             <div class="site-footer-content inner">
-                <section class="copyright"><a href="">{{@site.title}}</a> &copy; {{date format="YYYY"}}</section>
+                <section class="copyright"><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a> &copy; <?php echo date("Y"); ?></section>
                 <nav class="site-footer-nav">
-                    <a href="">Latest Posts</a>
-                    <a href="https://ghost.org" target="_blank" rel="noopener">Ghost</a>
+                    <a href="<?php echo home_url(); ?>">Latest Posts</a>
+                    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geist' ) ); ?>" target="_blank" rel="noopener">
+                        <?php
+                        /* translators: %s: CMS name, i.e. WordPress. */
+                        printf( esc_html__( 'Powered by %s', 'geist' ), 'WordPress' );
+                        ?>
+                    </a>
                 </nav>
             </div>
         </footer>
