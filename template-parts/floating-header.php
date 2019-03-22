e0ace8a07db65cfebd25c4097fb3535b04945124
Chris Yee <christophery@gmail.com>
2019-03-22 15:40:37 -0400

    adding sharing buttons

--- template-parts/floating-header.php
+++ template-parts/floating-header.php
@@ -1,3 +1,10 @@
+<?php
+
+$post_title = get_the_title();
+$post_url = get_the_permalink();
+
+?>
+
 <div class="floating-header">
     <div class="floating-header-logo">
         <a href="{{@site.url}}">
@@ -9,17 +16,17 @@
     </div>
     <span class="floating-header-divider">&mdash;</span>
     <div class="floating-header-title"><?php echo get_the_title(); ?></div>
-    <!-- <div class="floating-header-share">
-        <div class="floating-header-share-label">Share this {{> "icons/point"}}</div>
-        <a class="floating-header-share-tw" href="https://twitter.com/share?text={{encode title}}&amp;url={{url absolute="true"}}"
+    <div class="floating-header-share">
+        <div class="floating-header-share-label">Share this <?php get_template_part('template-parts/icons/point'); ?></div>
+        <a class="floating-header-share-tw" href="https://twitter.com/share?text=<?php echo $post_title; ?>&amp;url=<?php echo $post_url; ?>"
             onclick="window.open(this.href, 'share-twitter', 'width=550,height=235');return false;">
-            {{> "icons/twitter"}}
+            <?php get_template_part('template-parts/icons/twitter'); ?>
         </a>
-        <a class="floating-header-share-fb" href="https://www.facebook.com/sharer/sharer.php?u={{url absolute="true"}}"
+        <a class="floating-header-share-fb" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $post_url; ?>"
             onclick="window.open(this.href, 'share-facebook','width=580,height=296');return false;">
-            {{> "icons/facebook"}}
+            <?php get_template_part('template-parts/icons/facebook'); ?>
         </a>
-    </div> -->
+    </div>
     <!-- <progress id="reading-progress" class="progress" value="0">
         <div class="progress-container">
             <span class="progress-bar"></span>
