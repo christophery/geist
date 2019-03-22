51930784a9f7d79fd9b3aeceb3f8043c0053b858
Chris Yee <christophery@gmail.com>
2019-03-22 16:55:44 -0400

    updated credit

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
                 <section class="copyright"><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a> &copy; <?php echo date("Y"); ?></section>
                 <nav class="site-footer-nav">
                     <a href="<?php echo home_url(); ?>">Latest Posts</a>
-                    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geist' ) ); ?>" target="_blank" rel="noopener">
+                    <a href="<?php echo esc_url( __( 'https://chrisyee.ca', 'geist' ) ); ?>" target="_blank" rel="noopener">
                         <?php
                         /* translators: %s: CMS name, i.e. WordPress. */
-                        printf( esc_html__( 'Powered by %s', 'geist' ), 'WordPress' );
+                        printf( esc_html__( 'Geist by Chris Yee', 'geist' ), 'WordPress' );
                         ?>
                     </a>
                 </nav>
