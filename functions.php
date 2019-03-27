fad65d448afa5c56d89a1d68885c08f3d97fbc06
Chris Yee <christophery@gmail.com>
2019-03-27 14:38:40 -0400

    removed navigation.js

--- functions.php
+++ functions.php
@@ -127,8 +127,6 @@ function geist_scripts() {
 
 	wp_enqueue_script( 'geist-fitvids-js', get_template_directory_uri() . '/assets/js/jquery.fitvids.js', array(), '20190322', true );
 
-	wp_enqueue_script( 'geist-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'geist-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
