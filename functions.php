986f2f4e98cc4c01575e1c1e74e4da273910a208
Chris Yee <christophery@gmail.com>
2019-03-27 14:52:52 -0400

    updated content width

--- functions.php
+++ functions.php
@@ -94,7 +94,7 @@ function geist_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'geist_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'geist_content_width', 1040 );
 }
 add_action( 'after_setup_theme', 'geist_content_width', 0 );
 
