679aba5b2d28ba1fa15a22978336d575242e768b
Chris Yee <christophery@gmail.com>
2019-03-27 10:44:10 -0400

    replaced latest posts with related posts

--- template-parts/post-nav.php
+++ template-parts/post-nav.php
@@ -3,40 +3,49 @@
 $prevPost = get_previous_post();
 $nextPost = get_next_post();
 
+//get releated posts based on post category
+$related = new WP_Query(
+    array(
+        'category__in'   => wp_get_post_categories( $post->ID ),
+        'posts_per_page' => 3,
+        'post__not_in'   => array( $post->ID ),
+        'orderby'        => 'rand' //random order
+    )
+);
+
 ?>
 
 <aside class="read-next outer">
     <div class="inner">
         <div class="read-next-feed">
-            <article class="read-next-card">
-                <header class="read-next-card-header"
-                    <?php if ( get_header_image() ){ ?>
-                        style="background-image: url(<?php header_image(); ?>)
-                    <?php } ?>
-                ">
-                    <small class="read-next-card-header-sitetitle">&mdash; <?php echo get_bloginfo( 'name' ); ?> &mdash;</small>
-                    <h3 class="read-next-card-header-title"><a href="<?php echo home_url(); ?>"><?php _e('Latest Posts', 'geist'); ?></a></h3>
-                </header>
-                <div class="read-next-divider"><?php get_template_part('template-parts/icons/infinity'); ?></div>
-                <div class="read-next-card-content">
-                    <ul>
-                        <?php
-                            $args = array(
-                                'numberposts' => 3,
-                            );
-
-                            $recent_posts = wp_get_recent_posts( $args );
-                            foreach( $recent_posts as $recent ){
-                                echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
-                            }
-                            wp_reset_query();
-                        ?>
-                    </ul>
-                </div>
-                <footer class="read-next-card-footer">
-                    <a href="<?php echo home_url(); ?>"><?php _e('See all posts', 'geist'); ?> &#8594;</a>
-                </footer>
-            </article>
+            <?php if( $related->have_posts() ) { ?>
+                <article class="read-next-card">
+                    <header class="read-next-card-header"
+                        <?php if ( get_header_image() ){ ?>
+                            style="background-image: url(<?php header_image(); ?>)
+                        <?php } ?>
+                    ">
+                        <small class="read-next-card-header-sitetitle">&mdash; <?php echo get_bloginfo( 'name' ); ?> &mdash;</small>
+                        <h3 class="read-next-card-header-title"><a href="<?php echo home_url(); ?>"><?php _e('Related Posts', 'geist'); ?></a></h3>
+                    </header>
+                    <div class="read-next-divider"><?php get_template_part('template-parts/icons/infinity'); ?></div>
+                    <div class="read-next-card-content">
+                        <ul>
+                            <?php
+                                //output related posts
+                                while( $related->have_posts() ) {
+                                    $related->the_post();
+                                    echo '<li><a href="' . get_permalink() . '">' . get_the_title() .'</a> </li> ';
+                                }
+                                wp_reset_postdata();
+                            ?>
+                        </ul>
+                    </div>
+                    <footer class="read-next-card-footer">
+                        <a href="<?php echo home_url(); ?>"><?php _e('See all posts', 'geist'); ?> &#8594;</a>
+                    </footer>
+                </article>
+            <?php } ?>
 
             <!-- {{!-- If there's a next post, display it using the same markup included from - partials/post-card.hbs --}} -->
             <?php
