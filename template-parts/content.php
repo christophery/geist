37c9c61212f4ba07ef33c1db9d12bdad2afce55e
Chris Yee <christophery@gmail.com>
2019-03-27 10:49:46 -0400

    output first category name on index

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,7 +9,11 @@
 
 $author_avatar = get_avatar( get_the_author_meta( 'ID' ), 30, '', '', $args = array( 'class' => 'author-profile-image' ) );
 
+//get categories
 $categories = get_the_category();
+
+//get name of first category
+$category_name = $categories[0]->name;
 ?>
 
 <article class="post-card
@@ -29,13 +33,7 @@ $categories = get_the_category();
         <a class="post-card-content-link" href="<?php the_permalink(); ?>">
 
             <header class="post-card-header">
-                <?php
-                foreach($categories as $category){
-                	echo '<span class="post-card-tags">';
-                    echo $category->name . ' ';
-                    echo '</span>';
-                }
-                ?>
+                <span class="post-card-tags"><?php echo $category_name; ?></span>
                 <h2 class="post-card-title"><?php echo get_the_title(); ?></h2>
             </header>
 
