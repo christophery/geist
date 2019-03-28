5b69700ab0663b1f9f484e57d45cd2db7a90593b
Chris Yee <christophery@gmail.com>
2019-03-28 14:18:18 -0400

    renamed content-single-post to content-single

--- single.php
+++ single.php
@@ -20,7 +20,7 @@ get_header();
 while ( have_posts() ) :
     the_post();
 
-    get_template_part( 'template-parts/content-single-post' );
+    get_template_part( 'template-parts/content-single' );
 
 endwhile; // End of the loop.
 ?>
