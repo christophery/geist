4778b6695fe52b95d2fcd3f927929fb367878b2e
Chris Yee <christophery@gmail.com>
2019-03-29 11:36:35 -0400

    styled no search results

--- search.php
+++ search.php
@@ -10,8 +10,6 @@
 get_header();
 ?>
 
-<?php if ( have_posts() ) : ?>
-
 	<!-- {!--Special header.hbs partial to generate the <header> tag--}} -->
 	<?php get_template_part('template-parts/header'); ?>
 	    <div class="inner">
@@ -31,26 +29,28 @@ get_header();
 	    <div class="inner">
 	        <div class="post-feed">
 				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
+				if ( have_posts() ) :
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content');
+						/*
+						 * Include the Post-Type-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content');
 
-				endwhile;
+					endwhile;
 
-				the_posts_navigation();
+					the_posts_navigation();
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
 				?>
 	        </div>
 	    </div>
 	</main>
-
-<?php endif; ?>
 
 <?php
 get_footer();
--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -10,10 +10,6 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'geist' ); ?></h1>
-	</header><!-- .page-header -->
-
 	<div class="page-content">
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) :
