9b846e2ec8976e3ab88d6f9be66cdf414c8b1900
Chris Yee <christophery@gmail.com>
2019-04-01 12:15:33 -0400

    styled comments-list

--- comments.php
+++ comments.php
@@ -27,23 +27,7 @@ if ( post_password_required() ) {
 	if ( have_comments() ) :
 		?>
 		<h2 class="comments-title">
-			<?php
-			$geist_comment_count = get_comments_number();
-			if ( '1' === $geist_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'geist' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $geist_comment_count, 'comments title', 'geist' ) ),
-					number_format_i18n( $geist_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
+			<?php printf( __( 'Comments', 'geist' ) ); ?>
 		</h2><!-- .comments-title -->
 
 		<?php the_comments_navigation(); ?>
