f5f838609921b71131e6f9ca65a106357ab99204
Chris Yee <christophery@gmail.com>
2019-04-01 12:41:33 -0400

    updated comment avatar size + layout

--- comments.php
+++ comments.php
@@ -37,6 +37,7 @@ if ( post_password_required() ) {
 			wp_list_comments( array(
 				'style'      => 'ol',
 				'short_ping' => true,
+				'avatar_size'       => 60,
 			) );
 			?>
 		</ol><!-- .comment-list -->
