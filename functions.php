2a448d6a27f60a6939c2c67fad7d47d9fe35e465
Chris Yee <christophery@gmail.com>
2019-04-08 18:30:58 -0400

    reference js from build dir

--- functions.php
+++ functions.php
@@ -102,11 +102,11 @@ function geist_scripts() {
 	wp_enqueue_style( 'geist-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'geist-main-css', get_template_directory_uri() . '/built/screen.css', array(),'20190322' );
 
-	wp_enqueue_script( 'geist-main-js', get_template_directory_uri() . '/js/main.js', array('jquery'), '20190322', true );
+	wp_enqueue_script( 'geist-main-js', get_template_directory_uri() . '/built/js/main.js', array('jquery'), '20190322', true );
 
-	wp_enqueue_script( 'geist-fitvids-js', get_template_directory_uri() . '/js/jquery.fitvids.js', array(), '20190322', true );
+	wp_enqueue_script( 'geist-fitvids-js', get_template_directory_uri() . '/built/js/jquery.fitvids.js', array(), '20190322', true );
 
-	wp_enqueue_script( 'geist-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'geist-skip-link-focus-fix', get_template_directory_uri() . '/built/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
