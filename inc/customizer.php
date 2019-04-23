e018839dbc2a6473be4158559e60ce251d45b911
Chris Yee <christophery@gmail.com>
2019-04-23 12:55:28 -0400

    updated path to customizer.js

--- inc/customizer.php
+++ inc/customizer.php
@@ -174,7 +174,7 @@ function geist_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function geist_customize_preview_js() {
-	wp_enqueue_script( 'geist-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'geist-customizer', get_template_directory_uri() . '/built/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'geist_customize_preview_js' );
 
