5e2a6e0b46b1ca630f5bd5499f9e94e020f0f282
Chris Yee <christophery@gmail.com>
2020-10-01 09:25:06 -0400

    fixed path to fallback avatar

--- template-parts/byline-single.php
+++ template-parts/byline-single.php
@@ -14,7 +14,7 @@ $geist_author_url = get_author_posts_url( get_the_author_meta( 'ID' ), get_the_a
     <?php if( $geist_author_avatar ){ ?>
         <?php echo $geist_author_avatar; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
     <?php }else{ ?>
-        <span class="avatar-wrapper"><?php get_template_part('template-parts/avatar'); ?></span>
+        <span class="avatar-wrapper"><?php get_template_part('template-parts/icons/avatar'); ?></span>
     <?php } ?>
     <section class="author-card-content">
         <h4 class="author-card-name"><a href="<?php echo esc_url( $geist_author_url ); ?>"><?php echo esc_html( $geist_author_display_name ); ?></a></h4>
