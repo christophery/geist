0657f6a4df9d58cd8b9a2cbcc9fc505f83812c39
Christopher Yee <christophery@gmail.com>
2021-03-26 20:08:29 -0400

    added support for wp_body_open() #26

--- header.php
+++ header.php
@@ -21,4 +21,13 @@
 </head>
 <body <?php body_class(); ?>>
 
+	<?php
+		//check if wp_body_open function exists
+		if ( ! function_exists( 'wp_body_open' ) ) {
+		    function wp_body_open() {
+		        do_action( 'wp_body_open' );
+		    }
+		}
+	?>
+
     <div class="site-wrapper">
